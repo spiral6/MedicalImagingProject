a9b65f08b026740fba15128d037e3147443c8bb8
Shamee Mahmud <sami_safin_mahmud@hotmail.com>
2022-12-07 09:47:27 -0600

    Added OpenXR/HoloLens Support to project.

--- Assets/Scripts/SheppLoganPhantom.cs
+++ Assets/Scripts/SheppLoganPhantom.cs
@@ -37,15 +37,20 @@ public class SheppLoganPhantom : MonoBehaviour
             i++;
         }
 
+        GameObject phantom = new GameObject("Phantom");
+
         foreach(Ellipse p in Phantom)
         {
             GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             go.name = "RuntimeSphere" + p.name;
-            go.transform.position = new Vector3(p.center[0], p.center[1], p.center[2]);
+            go.transform.position = new Vector3(p.center[0], p.center[1], p.center[2] + 1.25f);
             go.transform.localScale = new Vector3(p.scale[0], p.scale[1], p.scale[2]);
             go.transform.eulerAngles = new Vector3(0, 0, p.theta);
             go.GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/Phantom" + p.name);
+            go.transform.SetParent(phantom.transform);
         }
+
+        phantom.transform.localScale = new Vector3(.5f, .5f, .5f);
     }
 
     // Update is called once per frame
